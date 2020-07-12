efb6e61f6f1c5db05f58d132ca8cfbbf13210aab
agnescameron <agnesfcameron@gmail.com>
2020-07-11 21:02:57 -0400

    searches for terms

--- tag_generation/insert_scripts.js
+++ tag_generation/insert_scripts.js
@@ -2,13 +2,28 @@
 
 
 //get the JSON
-let data = require('./database_dump.json');
+const data = require('./database_dump.json');
+
+
+const wp_posts = data.filter(table => table.name === 'wp_posts')[0].data;
+const wp_terms = data.filter(table => table.name === 'wp_terms')[0].data;
+const wp_term_taxonomy = data.filter(table => table.name === 'wp_term_relationships')[0].data;
+const wp_term_relationships = data.filter(table => table.name === 'wp_term_taxonomy')[0].data;
 
 //for each tag in tag_names
-data.forEach(table => console.log(table.name))
+wp_terms.forEach(term => {
+	wp_posts.forEach(post => {
+		if(post.post_title.includes(term.name)){
+			console.log(post.post_title, term.name)
+		}
+		else if(post.post_content.includes(term.name)){
+			console.log(post.post_title, term.name)
+		}
+
+	})
+})
 
 // search each 
-
 
 //post_title (if match then continue to id step)
 
