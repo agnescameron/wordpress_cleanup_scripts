696d4619ccabce13dc81017aac99401d260559e0
agnescameron <agnesfcameron@gmail.com>
2020-07-13 13:25:33 -0400

    cleaned up trailing commas

--- tag_generation/insert_scripts.js
+++ tag_generation/insert_scripts.js
@@ -1,5 +1,4 @@
-//file to create a number of insert scripts
-
+const fs = require('fs');
 
 //get the JSON
 const data = require('./database_dump.json');
@@ -11,12 +10,19 @@ const wp_term_taxonomy = data.filter(table => table.name === 'wp_term_relationsh
 const wp_term_relationships = data.filter(table => table.name === 'wp_term_taxonomy')[0].data;
 
 //perhaps some terms are so specific as to be included in full text, others just title, others not at all
-const not_searched = ["Main", "Presentations", "Publications"]
+const not_searched = ["Main", "Presentations", "Publications", "Uncategorized", "Publication", "Research", "Talks", "Teaching", "Blog",]
 const not_content = ["zone", "urban", "typology", "zines", "architecture", "workshop", "urbanism"]
 
-
 let id_map = [];
-let insert_string = 'INSERT IGNORE INTO `wp_term_relationships` (`object_id`, `term_taxonomy_id`) \n VALUES \n';
+
+//insert ignore doesn't insert duplicates
+let insert_string = 'INSERT IGNORE INTO `wp_term_relationships` (`object_id`, `term_taxonomy_id`) VALUES ';
+
+
+// delete if fuck up
+// let insert_string = 'DELETE FROM `wp_term_relationships` WHERE (`object_id`, `term_taxonomy_id`) IN (';
+
+
 //perhaps also posts from last 4 years?
 
 
@@ -41,21 +47,17 @@ wp_terms.forEach(term => {
 	}
 })
 
-// console.log(id_map)
-
 //if there's a match, then get the id of both the tag and the post
 id_map.forEach(rship => {
-	const insert_statement = `(${rship.object_id}, ${rship.term_taxonomy_id}),\n`;
+	const insert_statement = `(${rship.object_id}, ${rship.term_taxonomy_id}), `;
 	insert_string = insert_string + insert_statement;
 })
 
 insert_string = insert_string.slice(0, -2) + ';'
+console.log('id map contains', id_map.length, 'entries')
 
-console.log(insert_string);
 //if those have been found successfully, formulate an insert statement
-
-
-//insert statement: check that the relationship does not already exist. 
-
-
-//if it does not, then insert
+fs.writeFile('insert_statement.txt', insert_string, function (err) {
+  if (err) return console.log(err);
+  console.log('successfully created insert statement');
+});
